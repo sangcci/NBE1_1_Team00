8f2ec74b5252a5c22e33c55cfce1f03d6e269793
sangcci <pshsh910@gmail.com>
2024-09-09 12:04:29 +0900

    fix: 주문 생성 시 주문 상품 정보를 확인하는 로직 추가

--- src/main/java/nbe1/team08/gridscircles/order/service/OrderService.java
+++ src/main/java/nbe1/team08/gridscircles/order/service/OrderService.java
@@ -8,7 +8,13 @@ import nbe1.team08.gridscircles.common.exception.AccessDeniedException;
 import nbe1.team08.gridscircles.order.domain.Order;
 import nbe1.team08.gridscircles.order.domain.OrderItem;
 import nbe1.team08.gridscircles.order.domain.Orderer;
+import nbe1.team08.gridscircles.order.service.dto.OrderCancelCommand;
+import nbe1.team08.gridscircles.order.service.dto.OrderCreateCommand;
+import nbe1.team08.gridscircles.order.service.dto.OrderCreateCommand.OrderItemCreate;
+import nbe1.team08.gridscircles.order.service.dto.OrderDetailResponse;
 import nbe1.team08.gridscircles.order.service.port.OrderRepository;
+import nbe1.team08.gridscircles.product.domain.Product;
+import nbe1.team08.gridscircles.product.service.port.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,43 +23,78 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
 
     @Transactional(readOnly = true)
-    public List<Order> findAll() {
-        return orderRepository.findAll();
+    public List<OrderDetailResponse> findAll() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream()
+                .map(OrderDetailResponse::from)
+                .toList();
     }
 
     @Transactional(readOnly = true)
-    public List<Order> findAllByEmail(String email) {
-        return orderRepository.findAllByEmail(email);
+    public List<OrderDetailResponse> findAllByEmail(String email) {
+        List<Order> orders = orderRepository.findAllByEmail(email);
+        return orders.stream()
+                .map(OrderDetailResponse::from)
+                .toList();
     }
 
     @Transactional(readOnly = true)
-    public Order findById(UUID uuid) {
-        return orderRepository.findById(uuid)
-                .orElseThrow(() -> new NoSuchElementException("Order not found"));
-    }
-
-    @Transactional
-    public UUID createOrder(Orderer orderer, List<OrderItem> orderItems) {
-        Order order = Order.create(orderer, orderItems);
-
-        UUID savedUuid = orderRepository.save(order);
-        return savedUuid;
-    }
-
-    @Transactional
-    public void cancelOrder(UUID uuid, String email) {
+    public OrderDetailResponse findById(UUID uuid) {
         Order order = orderRepository.findById(uuid)
                 .orElseThrow(() -> new NoSuchElementException("Order not found"));
+        return OrderDetailResponse.from(order);
+    }
 
-        if (!order.getOrderer().isMine(email)) {
+    @Transactional
+    public UUID createOrder(OrderCreateCommand orderCreateCommand) {
+        // 주문 상품 정보 확인
+        List<OrderItemCreate> orderItemCreates = orderCreateCommand.orderItemCreates();
+        if (orderItemCreates.isEmpty()) {
+            throw new NullPointerException("Not write item lists");
+        }
+        List<OrderItem> orderItems = orderItemCreates.stream()
+                .map(orderItemCreate -> {
+                    Product product = productRepository.findById(orderItemCreate.productId())
+                            .orElseThrow(() -> new NoSuchElementException("Doesn't exist product"));
+                    return new OrderItem(
+                            0L,
+                            product.getCategory(),
+                            product.getPrice(),
+                            orderItemCreate.quantity(),
+                            null,
+                            product);
+                })
+                .toList();
+        // 주문자 정보 확인
+        Orderer orderer = new Orderer(
+                orderCreateCommand.email(),
+                orderCreateCommand.address(),
+                orderCreateCommand.postcode()
+        );
+        // 주문 생성
+        Order order = Order.create(orderer, orderItems);
+
+        // 영속화
+        return orderRepository.save(order);
+    }
+
+    @Transactional
+    public void cancelOrder(OrderCancelCommand orderCancelCommand) {
+        // 주문 존재 확인
+        Order order = orderRepository.findById(orderCancelCommand.uuid())
+                .orElseThrow(() -> new NoSuchElementException("Order not found"));
+        // 주문 데이터가 자신의 것이 아닐 경우 예외 발생
+        if (!order.getOrderer().isMine(orderCancelCommand.email())) {
             throw new AccessDeniedException("Can't access order");
         }
-
+        // 배송 중일 경우 예외 발생
         if (!order.canCancelOrder()) {
             throw new IllegalStateException("Can't cancel order cause already delivering");
         }
+
         order.cancel();
     }
 }
